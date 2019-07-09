cc274ba9c5edbe5b56cc2b9ad03e5ae9879797a5
Alisia Michel <cc0-11@outlook.de>
2019-07-09 11:31:19 +0200

    authentication made necessary by default, deactivated for spaces index and show

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::Base
+  #Before every method can be called a log in is necessary (can be deactivated with: skip_before_action :authenticate_user!, only:NAME_OF_METHOD )
+  before_action :authenticate_user!
 end
--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -1,4 +1,7 @@
 class SpacesController < ApplicationController
+  #for more info about this go to application_controller or sources from yesterday!
+  #if a method is written here you DO NOT NEED TO LOG IN to call it!
+  skip_before_action :authenticate_user!, only: [:index, :show]
   def index
   end
 
