88649fae2ae9c0642f513271064df2c72ba75657
Paul Kathmann <paul.kathmann.98@gmail.com>
2019-07-09 12:36:41 +0200

    Followed Kitts guide to include the pundit. Installed the Policies for space and booking and also did a migration to add an admin boolean to our User model like in the Kitt guide. However when the views are created they still need to implement the policy part for each view individually like in the Guide From Kitt 'Authorization & Pundit'.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,18 @@
 class ApplicationController < ActionController::Base
   #Before every method can be called a log in is necessary (can be deactivated with: skip_before_action :authenticate_user!, only:NAME_OF_METHOD )
   before_action :authenticate_user!
+  include Pundit
+
+  # Pundit: white-list approach.
+  after_action :verify_authorized, except: :index, unless: :skip_pundit?
+  after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
+
+  # Uncomment when you *really understand* Pundit!
+  # rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  # def user_not_authorized
+  #   flash[:alert] = "You are not authorized to perform this action."
+  #   redirect_to(root_path)
+  # end
 
   def configure_permitted_parameters
     # First and Last name field for Log-in
@@ -8,5 +20,11 @@ class ApplicationController < ActionController::Base
 
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
+
+   private
+
+  def skip_pundit?
+    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
 end
