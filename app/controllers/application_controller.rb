08089138f117f6dc932a50fff67b16fba6291382
Alisia Michel <cc0-11@outlook.de>
2019-07-09 11:47:19 +0200

    Add username to user identification

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   #Before every method can be called a log in is necessary (can be deactivated with: skip_before_action :authenticate_user!, only:NAME_OF_METHOD )
   before_action :authenticate_user!
+
+  def configure_permitted_parameters
+    # First and Last name field for Log-in
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
 end
