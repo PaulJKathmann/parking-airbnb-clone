7157c959733d7251027f51ef0d39e55a71970db6
Paul Kathmann <paul.kathmann.98@gmail.com>
2019-07-10 10:39:17 +0200

    Made title dynamic and updated button link

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
   resources :spaces
-  get 'bookings/index'
-  get 'bookings/show'
-  get 'bookings/new'
-  get 'bookings/create'
-  get 'bookings/update'
-  get 'bookings/edit'
-
+  resources :bookings, only: [:index, :show, :new, :create, :update, :edit]
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
