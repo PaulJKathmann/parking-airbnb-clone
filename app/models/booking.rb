10c72809b5f669e8f3a0547f4225feccd6355546
Alisia Michel <cc0-11@outlook.de>
2019-07-11 12:55:13 +0200

    Inside of spaces time frame validation for booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,15 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :space
+  validates :start_date, presence: true, if: :overlap?
+  # Valdiation that dates don't overlap while booking
+  def overlap?
+    range = Range.new start_date, end_date
+    overlaps = Space.where(id: self.space).in_range(range)
+    overlap_error if overlaps.empty?
+  end
+
+  def overlap_error
+    errors.add(:overlap_error, 'Your request is outside of the offered booking times!')
+  end
 end
--- app/models/space.rb
+++ app/models/space.rb
@@ -4,4 +4,8 @@ class Space < ApplicationRecord
   mount_uploader :photo, PhotoUploader
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+
+  scope :in_range, -> range {
+  where('(start_date <= ? and ? <= ? and end_date >= ?)',range.first, range.first, range.last, range.last)
+  }
 end
