2c5adacf5aa7cd57f9a26692086c633f0342daaa
Lu Zhu <luzhu000@gmail.com>
2019-07-12 12:59:43 +0200

    update the show page

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -6,6 +6,11 @@ class BookingsController < ApplicationController
 
   def show
     @booking = Booking.find(params[:id])
+    @markers =
+      [{
+        lat: @booking.space.latitude,
+        lng: @booking.space.longitude,
+      }]
     authorize @booking
   end
 
